271feee22b7f0d4bbb8c70efbf5ddf83488f425c
GitHub63Jcmc <jcmcgoojcmc@gmail.com>
2024-09-03 17:21:00 +0200

    commit Joao 11

--- js/localStorege.js
+++ js/localStorege.js
@@ -1,23 +1,91 @@
 
 
-window.onload = function() {
+window.onload = leerPerferencias;
 
+function  leerPerferencias(){
+
+    //Nombre de usuario
     if(localStorage.getItem("nombreUsuario")) {
         saludo.innerHTML = "Hola, " +localStorage.getItem("nombreUsuario");
-        
+                
     }else{
         nombre = prompt("Como te llamas");
         localStorage.setItem("nombreUsuario", nombre);
     }
+
+    // ver se hay valores guardados en localStorage
+    if(localStorage.getItem("tamanoTexto")&&localStorage.getItem("colorTexto")&&localStorage.getItem("fuenteTexto")) {
+
+        let tamanoFinal = localStorage.getItem("tamanoTexto");
+        let colorFinal = localStorage.getItem("colorTexto");
+        let fuenteFinal = localStorage.getItem("fuenteTexto");
+        parrafo.style.fontSize= tamanoFinal +"px";
+        parrafo.style.color= colorFinal;
+        parrafo.style.fontFamily= fuenteFinal;
+
+        //aqui se leen las últimas perferencias duardadas
+        tamano.value = tamanoFinal;
+        color.value = colorFinal;
+        fuente.value = fuenteFinal;
+
+        mensaje.innerHTML = "!!! Mostrando Tus Perferencias !!!";
+    }
 }
 
-function guardar() {
-    localStorage.setItem("tamanoFuente", tamano.value);
-    localStorage.setItem("colorFuente", color.value);
-    parrafo.style.fontSize = tamano.value + "px";
-    parrafo.style.color = color.value;
-    localStorage.setItem("parrafo", color.value);
+function guardarPerferencias(){
+    let tamanoFinal = tamano.value;
+    let colorFinal = color.value;
+    let fuenteFinal = fuente.options[fuente.selectedIndex].text;
+
+    switch (fuenteFinal){
+        case 1:
+            `${fuenteFinal}`
+            break;
+
+        case 2:
+            `${fuenteFinal}`
+            break;
+
+        case 3:
+            `${fuenteFinal}`
+            break;
+
+        default:
+            "Fuente del Sistema"
+    }
+
+    alert(`Fuente Elegida: ${fuenteFinal}`);
+
+    localStorage.setItem("tamanoTexto", tamanoFinal);
+    localStorage.setItem("colorTexto", colorFinal);
+    localStorage.setItem("fuenteTexto", fuenteFinal);
+    parrafo.style.fontSize= tamanoFinal +"px";
+    parrafo.style.color= colorFinal;
+    tamano.value = tamanoFinal;
+    color.value = colorFinal;
+    mensaje.innerHTML = "Mostrando Tus Perferencias";
+
 }
+
+
+// window.onload = function() {
+
+//     if(localStorage.getItem("nombreUsuario")) {
+//         saludo.innerHTML = "Hola, " +localStorage.getItem("nombreUsuario");
+        
+//     }else{
+//         nombre = prompt("Como te llamas");
+//         localStorage.setItem("nombreUsuario", nombre);
+//     }
+// }
+
+// function guardar() {
+//     localStorage.setItem("tamanoFuente", tamano.value);
+//     localStorage.setItem("colorFuente", color.value);
+//     parrafo.style.fontSize = tamano.value + "px";
+//     parrafo.style.color = color.value;
+//     localStorage.setItem("parrafo", color.value);
+// }
 
 
 // ---------------------------------------------------
--- js/pelota.js
+++ js/pelota.js
@@ -1,40 +1,39 @@
 
 posicion=0;
-        vueltas=0;
-        
-        window.onload=function(){
-            intervalo=setInterval(mover,5);
-            }
-        function mover(){
-            bolita.style.left=posicion+"px";
-            posicion++;
-            if (posicion==innerWidth-300){
-                clearInterval(intervalo)
-                otroIntervalo=setInterval(mover2,5)
-            }
-        } 
-        
-        function mover2(){
-            bolita.style.left=posicion+"px";
-            posicion--;
-            if (posicion==0){
-                vueltas++;
-                num.innerHTML=vueltas;
-                clearInterval(otroIntervalo)
-            if (vueltas<3)intervalo=setInterval(mover,5)
-            }    
-        }  
+vueltas=0;
 
-        function reiniciar() {
-            if (vueltas==3) {
-                intervalo=setInterval(mover,5);
-            vueltas=0;
-            
-            //Actualizar el contador de vueltas en la pantalla
-            num.innerHTML=vueltas;
-            
-        }
-        else{
-            advertencia.innerHTML="Se esta moviendo"
-        }
+window.onload=function(){
+    intervalo=setInterval(mover,5);
+}
+
+function mover(){
+    bolita.style.left=posicion+"px";
+    posicion++;
+    if (posicion==innerWidth-300){
+        clearInterval(intervalo)
+        otroIntervalo=setInterval(mover2,5)
     }
+} 
+
+function mover2(){
+    bolita.style.left=posicion+"px";
+    posicion--;
+    if (posicion==0){
+        vueltas++;
+        num.innerHTML=vueltas;
+        clearInterval(otroIntervalo)
+    if (vueltas<3)intervalo=setInterval(mover,5)
+    }    
+} 
+
+function reiniciar() {
+    if (vueltas==3) {
+        intervalo=setInterval(mover,5);
+        vueltas=0;
+    
+        //Actualizar el contador de vueltas en la pantalla
+        num.innerHTML=vueltas;
+    }else{
+        advertencia.innerHTML="Se esta moviendo"
+    }
+}
